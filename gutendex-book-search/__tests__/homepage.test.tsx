ea253ae8028ee3a7da544758547c6bd092645254
Theo <theo.osinfolarin@gmail.com>
2025-10-24 15:24:22 +0100

    findbooks unit test

--- gutendex-book-search/__tests__/homepage.test.tsx
+++ gutendex-book-search/__tests__/homepage.test.tsx
@@ -12,18 +12,15 @@ describe('homepage', () => {
         const greeting = screen.getByText(/Welcome to the Gutendex Book Search/i)
         expect(greeting).toBeInTheDocument();
         const link = screen.getByRole('linkToBookSearch');
-        expect(link).toBeInTheDocument();
-        
+        expect(link).toBeInTheDocument();    
     });
-    it.skip('the welcome message link should take you to the findbooks page', async () => {
+    it('the welcome message link should take you to the findbooks page', async () => {
       render(<Homepage />);  
       const greeting = screen.getByText(/Welcome to the Gutendex Book Search/i)
       expect(greeting).toBeInTheDocument();
       const link = screen.getByRole('linkToBookSearch');
       expect(link).toBeInTheDocument();
-      // await userEvent.click(link);
-      // screen.logTestingPlaygroundURL()
-      // const bookSearchHeading = await screen.findByText(/Search for a book of your choice!/i);
-      // expect(bookSearchHeading).toBeInTheDocument();
+      expect(link).toHaveAttribute('href', 'findBooks');
+      
     });
 });
